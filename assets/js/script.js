0a03e2509b5215aebc89e7b0eab01bd779ad6522
joyerin0309-code <joyerin0309@gmail.com>
2025-12-07 22:26:38 +0900

    llll

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,14 @@ $(function () {
     $('.header-nav a').click(function () {
         $('.header-nav').removeClass('on');
     });
+
+    $(".privacy-open").on("click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(".privacy-popup").show();
+    });
+
+    $(".privacy-popup__close").on("click", function () {
+        $(".privacy-popup").hide();
+    });
 });
